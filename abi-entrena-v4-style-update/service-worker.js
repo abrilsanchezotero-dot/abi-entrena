9f2d3330f93d025748a1423935058e74c76bb1c8
abrilsanchezotero-dot <abrilsanchezotero@gmail.com>
2026-06-23 21:26:08 -0300

    Add files via upload

---
+++ abi-entrena-v4-style-update/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "abi-entrena-v4-aesthetic";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./routine-data.js",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png",
+  "./assets/sticker-dumbbell.svg",
+  "./assets/sticker-star.svg",
+  "./assets/sticker-cherry.svg",
+  "./assets/sticker-lightning.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
